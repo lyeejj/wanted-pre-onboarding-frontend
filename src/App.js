bd3efe7ecebd604d2414778a10a31562faf75528
lyeejj <lyshine012@gmail.com>
2023-08-06 14:29:17 +0900

    Feat: 로그인 여부에 따른 리다이렉트 처리 구현

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Todo from "./pages/Todo";
+import AuthRoute from "./components/AuthRoute";
+import PublicRoute from "./components/PublicRoute";
 
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/todo" element={<Todo />} />
+      <Route element={<PublicRoute />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Route>
+      <Route element={<AuthRoute />}>
+        <Route path="/todo" element={<Todo />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
