a7104f6e238f512d6510383853eb4f8cf9530b55
lyeejj <lyshine012@gmail.com>
2023-08-06 13:32:45 +0900

    Feat: 회원가입 SignUp 기능 구현

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,16 +1,47 @@
 import { Link } from "react-router-dom";
+import useInputs from "../hooks/useInputs";
+import { signup } from "../lib/apis/auth";
 
 const SignUp = () => {
+  const initialValue = { email: "", password: "" };
+  const { formData, handleChange, reset } = useInputs(initialValue);
+  const { email, password } = formData;
+
+  const validateBtn = !(email.includes("@") && password.length >= 8);
+
+  const handleSignUpSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    signup(formData);
+    reset();
+  };
   return (
     <>
       <h1>회원가입</h1>
-      <form>
-        <input data-testid="email-input" type="email" />
-        <input data-testid="password-input" type="password" />
-        <input data-testid="signup-button" type="submit" value={"회원가입"} />
+      <form onSubmit={handleSignUpSubmit}>
+        <input
+          data-testid="email-input"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          value={email}
+          placeholder="이메일 아이디"
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          onChange={handleChange}
+          value={password}
+          placeholder="비밀번호"
+        />
+        <button data-testid="signup-button" disabled={validateBtn}>
+          회원가입
+        </button>
       </form>
+      <span>이미 계정이 있으신가요?</span>
       <span>
-        <Link to="/signin">로그인하기</Link>
+        <Link to="/signin">로그인</Link>
       </span>
     </>
   );
