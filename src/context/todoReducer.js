cd01f7d82398767dbb960b53253fee70134a956c
lyeejj <lyshine012@gmail.com>
2023-08-07 10:29:19 +0900

    Feat: todoReducer 리듀서 함수 생성

---
+++ src/context/todoReducer.js
@@ -0,0 +1,30 @@
+export const todoReducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_TODOS":
+      return { ...state, todos: action.payload };
+    case "ADD_TODO":
+      return { ...state, todos: [...state.todos, action.payload] };
+    case "TOGGLE_TODO":
+      return state.todos.map((todo) =>
+        todo.id === action.payload
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo
+      );
+    case "REMOVE_TODO":
+      return {
+        ...state,
+        todos: state.todos.filter((todo) => todo.id !== action.payload),
+      };
+    case "UPDATE_TODO":
+      return {
+        ...state,
+        todos: state.todos.map((todo) =>
+          todo.id === action.payload.id
+            ? { ...todo, todo: action.payload.updateTodo }
+            : todo
+        ),
+      };
+    default:
+      return state;
+  }
+};
