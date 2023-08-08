835364d07810a3b728c4b2f8972f09cc2bd03981
lyeejj <lyshine012@gmail.com>
2023-08-09 00:29:09 +0900

    Fix: todo api 함수 리턴값 변경

--- src/lib/apis/todo.js
+++ src/lib/apis/todo.js
@@ -1,17 +1,20 @@
 import axiosInstance from "./instance";
 
-export const createTodo = (data) => {
-  return axiosInstance.post("/todos", data);
+export const createTodo = async (data) => {
+  const response = await axiosInstance.post("/todos", data);
+  return response.data;
 };
 
-export const getTodos = () => {
-  return axiosInstance.get("/todos");
+export const getTodos = async () => {
+  const response = await axiosInstance.get("/todos");
+  return response.data;
 };
 
-export const updateTodo = (id, data) => {
-  return axiosInstance.put(`/todos/${id}`, data);
+export const updateTodo = async (id, data) => {
+  const response = await axiosInstance.put(`/todos/${id}`, data);
+  return response.data;
 };
 
-export const deleteTodo = (id) => {
-  return axiosInstance.delete(`/todos/${id}`);
+export const deleteTodo = async (id) => {
+  await axiosInstance.delete(`/todos/${id}`);
 };
