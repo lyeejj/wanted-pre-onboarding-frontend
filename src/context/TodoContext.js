a73a1bd3d55b26102768a72ec8c8b13c20de3f6a
lyeejj <lyshine012@gmail.com>
2023-08-09 00:35:22 +0900

    Feat: TodoContext, dispatch를 사용한 crud 함수 정의

--- src/context/TodoContext.js
+++ src/context/TodoContext.js
@@ -1,18 +1,70 @@
-import { createContext, useReducer, useContext, useRef } from "react";
+import { createContext, useReducer, useContext } from "react";
 import { todoReducer } from "./todoReducer";
+import { getTodos, createTodo, updateTodo, deleteTodo } from "../lib/apis/todo";
 
 const initialState = {
   todos: [],
 };
 
-const TodoContext = createContext();
+export const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
   const [state, dispatch] = useReducer(todoReducer, initialState);
-  const nextId = useRef(1);
+
+  const fetchTodos = async () => {
+    try {
+      const todos = await getTodos();
+      dispatch({ type: "FETCH_TODOS", payload: todos });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      }
+    }
+  };
+
+  const handleAddTodo = async (todoInput) => {
+    try {
+      const newTodo = await createTodo(todoInput);
+      dispatch({ type: "FETCH_TODOS", payload: newTodo });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      }
+    }
+  };
+
+  const handleUpdateTodo = async (id, data) => {
+    try {
+      const updatedTodo = await updateTodo(id, data);
+      dispatch({ type: "UPDATE_TODO", payload: { id, updatedTodo } });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      }
+    }
+  };
+
+  const handleDeleteTodo = async (id) => {
+    try {
+      await deleteTodo(id);
+      dispatch({ type: "REMOVE_TODO", payload: id });
+    } catch (error) {
+      if (error.response) {
+        alert(error.response.data.message);
+      }
+    }
+  };
 
   return (
-    <TodoContext.Provider value={{ state, dispatch, nextId }}>
+    <TodoContext.Provider
+      value={{
+        todos: state.todos,
+        fetchTodos,
+        handleAddTodo,
+        handleUpdateTodo,
+        handleDeleteTodo,
+      }}
+    >
       {children}
     </TodoContext.Provider>
   );
