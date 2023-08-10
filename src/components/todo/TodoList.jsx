6bb901097db642200c209171327f3c1e660130a5
lyeejj <lyshine012@gmail.com>
2023-08-10 19:35:21 +0900

    Feat: TodoList 컴포넌트 Todo page에 적용

---
+++ src/components/todo/TodoList.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { useTodoContext } from "../../context/TodoContext";
+import TodoItem from "./TodoItem";
+
+const TodoList = () => {
+  const [todoInput, setTodoInput] = useState("");
+  const { todos, fetchTodos, handleAddTodo } = useTodoContext();
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const handleChange = (e) => {
+    setTodoInput(e.target.value);
+  };
+
+  const onClickAddTodo = (e) => {
+    e.preventDefault();
+    handleAddTodo({ todo: todoInput });
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={onClickAddTodo}>
+        <input
+          data-testid="new-todo-input"
+          id="new-todo-input"
+          name="new-todo-input"
+          value={todoInput}
+          onChange={handleChange}
+        />
+        <button data-testid="new-todo-add-button" type="submit">
+          추가
+        </button>
+      </form>
+      <ul>
+        {todos.length &&
+          todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
+      </ul>
+    </>
+  );
+};
+export default TodoList;
--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -1,44 +1,7 @@
-import { useState, useEffect } from "react";
-import { useTodoContext } from "../context/TodoContext";
-import TodoItem from "../components/todo/TodoItem";
+import TodoList from "../components/todo/TodoList";
 
 const Todo = () => {
-  const [todoInput, setTodoInput] = useState("");
-  const { todos, fetchTodos, handleAddTodo } = useTodoContext();
-
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  const handleChange = (e) => {
-    setTodoInput(e.target.value);
-  };
-
-  const onClickAddTodo = (e) => {
-    e.preventDefault();
-    handleAddTodo({ todo: todoInput });
-  };
-
-  return (
-    <>
-      <h1>Todo List</h1>
-      <form onSubmit={onClickAddTodo}>
-        <input
-          data-testid="new-todo-input"
-          id="new-todo-input"
-          name="new-todo-input"
-          value={todoInput}
-          onChange={handleChange}
-        />
-        <button data-testid="new-todo-add-button" type="submit">
-          추가
-        </button>
-      </form>
-      <ul>
-        {todos.length &&
-          todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
-      </ul>
-    </>
-  );
+  return <TodoList />;
 };
+
 export default Todo;
