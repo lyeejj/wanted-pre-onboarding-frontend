c2f0a1cd07aa9af500959ce7552404717d3e56ea
lyeejj <lyshine012@gmail.com>
2023-08-10 20:20:00 +0900

    Style: TodoListContainer 스타일 수정

--- src/components/todo/TodoStyles.js
+++ src/components/todo/TodoStyles.js
@@ -43,7 +43,30 @@ export const TodoForm = styled.form`
 
 export const TodoListContainer = styled.ul`
   padding: 16px;
-  width: 500px;
+  width: 340px;
   height: 300px;
   overflow: auto;
+
+  li {
+    margin: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: start;
+    font-size: 1.1rem;
+  }
+
+  button {
+    margin: 0 5px;
+    padding: 2px 6px;
+    border: 1px solid #1fb845;
+    border-radius: 8px;
+    font-size: 14px;
+    color: #1fb845;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: #1fb845;
+    color: #fff;
+  }
 `;
