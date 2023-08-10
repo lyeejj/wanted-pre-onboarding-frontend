c2f261fb698da9c21690e62d49e6f1dc85acd162
lyeejj <lyshine012@gmail.com>
2023-08-10 20:47:47 +0900

    Style: Home 화면 스타일 수정

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,35 @@
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 
 const Home = () => {
   return (
     <>
-      <Link to="/signin">시작하기</Link>
+      <StyledLink to="/signin">
+        <span>시작하기</span>
+      </StyledLink>
     </>
   );
 };
+
+const StyledLink = styled(Link)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1.5rem;
+
+  span {
+    padding: 16px 20px;
+    transition: 0.3s ease-in-out;
+    border: 1px solid #2a2a2a;
+    border-radius: 6px;
+
+    &:hover {
+      border: none;
+      background-color: #1fb845;
+      color: #fff;
+    }
+  }
+`;
 
 export default Home;
