58ea1d726f681037934172277f9e559434b02d77
lyeejj <lyshine012@gmail.com>
2023-08-14 13:47:16 +0900

    Fix: 환경변수 사용 제거

--- src/lib/apis/instance.js
+++ src/lib/apis/instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: "https://www.pre-onboarding-selection-task.shop",
+  // baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
